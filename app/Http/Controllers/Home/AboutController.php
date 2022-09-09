6b139a6725d6a1e5264564f5f8a8590b0f11df5a
Than Htike Aung <phoehtet25@gmail.com>
2022-09-09 20:04:46 +0630

    multi image finished

--- app/Http/Controllers/Home/AboutController.php
+++ app/Http/Controllers/Home/AboutController.php
@@ -80,6 +80,53 @@ class AboutController extends Controller
                 'message' => 'Multi image inserted Successfully',
                 'alert-type' => 'success'
             );
+        return redirect()->route('all.multi.image')->with($notification);
+    }
+
+    public function AllMultiImage(){
+        $allMultiImage = MultiImage::all();
+        return view('admin.about_page.all_multiimage', compact('allMultiImage'));
+    }
+
+    public function EditMultiImage($id){
+        $multiImage = MultiImage::findOrFail($id);
+        return view('admin.about_page.edit_multi_image', compact('multiImage'));
+    }
+
+    public function UpdateMultiImage(Request $request){
+        $multi_image_id = $request->id;
+
+        if($request->file('multi_image')){
+            $image = $request->file('multi_image');
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+            Image::make($image)->resize(220, 220)->save('upload/multi/' . $name_gen);
+            $save_url = 'upload/multi/' . $name_gen;
+
+            MultiImage::findOrFail($multi_image_id)->update([
+                'multi_image' => $save_url,
+            ]);
+
+        }
+          $notification = array(
+                'message' => 'Multi image updated Successfully',
+                'alert-type' => 'success'
+            );
+        return redirect()->route('all.multi.image')->with($notification);
+    }
+
+    public function DeleteMultiImage($id){
+        $multi = MultiImage::findOrFail($id);
+        $img = $multi->multi_image;
+        unlink($img);
+
+        MultiImage::findOrFail($id)->delete();
+
+
+          $notification = array(
+                'message' => 'Multi image deleted Successfully',
+                'alert-type' => 'success'
+            );
         return redirect()->back()->with($notification);
+
     }
 }
--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -124,6 +124,19 @@
 
         <!-- init js -->
         <script src="{{ asset('backend/assets/js/pages/form-editor.init.js') }}"></script>
+
+
+          <!-- Required datatable js -->
+        <script src="{{ asset('backend/assets/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
+        <script src="{{ asset('backend/assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+ <script src="{{ asset('backend/assets/js/code.js') }}"></script>
+
+
+
     </body>
 
 </html>
